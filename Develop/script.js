3d8d65c9fa1825afafd59071eccebb3a2d4a5269
Annie Hoang <anniehoang.anh@gmail.com>
2022-05-18 15:46:08 -0400

    added alpha-numeric and special criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,3 @@
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
 
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
@@ -15,7 +9,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var uppercase = lowercase.toUpperCase()
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var number = "0123456789"
 var special = "!@#$%^&*"
 
@@ -46,7 +40,7 @@ function generatePassword(){
     console.log(isUppercase)
     var isNumber= confirm("Include number?")
     console.log(isNumber)
-    var isSpecial= confirm("Include special characters?")
+    var isSpecial= confirm("Include special character?")
     console.log(isSpecial)
 
     if(isLowercase===true){
@@ -54,6 +48,22 @@ function generatePassword(){
       console.log(positionString)
       finalPwd = finalPwd + lowercase[positionString]
     }
+    if(isUppercase===true){
+      var positionString=Math.floor(Math.random() *26) 
+      console.log(positionString)
+      finalPwd = finalPwd + uppercase[positionString]
+    }
+    if(isNumber===true){
+      var positionString=Math.floor(Math.random() *10) 
+      console.log(positionString)
+      finalPwd = finalPwd + number[positionString]
+    }
+    if(isSpecial===true){
+      var positionString=Math.floor(Math.random() *8) 
+      console.log(positionString)
+      finalPwd = finalPwd + special[positionString]
+    }
+
   }
   
     
